97530738cd135572b32e8d4e95e71aa18435c6b9
Pedro Losas <pedrolosas@MacBook-Pro-de-Pedro.local>
2023-10-18 21:46:48 +0200

    DI in checkservice

--- dist/presenters/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Server = void 0;
-class Server {
-    static start() {
-        console.log('Server started');
-    }
-}
-exports.Server = Server;
--- src/presenters/server.ts
+++ src/presenters/server.ts
@@ -1,10 +1,15 @@
-import { CronJob } from 'cron';
+import { CheckService } from '../domain/use-cases/checks/check.service';
 import { CronService } from './cron/cron.service';
 
 export class Server {
   public static start() {
+    const url = 'http://localhost:3000';
+
     CronService.createCron('*/5 * * * * *', () => {
-      console.log('Cron job executed');
+      new CheckService(
+        () => console.log(`CheckService use case ${url} is ok`),
+        (error) => console.error(error)
+      ).execute(url);
     });
   }
 }
